8d10a11b941217ff6beed1fafa89ab7317b5dbd0
bbmrn <68998700+bbmrn@users.noreply.github.com>
2025-03-02 18:25:04 +0530

    Update mongo.go

--- src/database/mongo.go
+++ src/database/mongo.go
@@ -15,6 +15,20 @@ import (
 var MongoClient *mongo.Client
 var TransactionCollection *mongo.Collection
 
+// InitMongoDB initializes a connection to MongoDB using environment configurations.
+// It performs the following steps:
+// 1. Creates a context with 10 second timeout
+// 2. Establishes connection to MongoDB using MONGO_URL from environment (defaults to localhost:27017)
+// 3. Verifies connection by pinging the database
+// 4. Initializes the transaction collection reference
+//
+// The function will terminate the program with a fatal error if:
+// - Unable to establish connection to MongoDB
+// - Unable to ping the MongoDB server
+//
+// Global variables set:
+// - MongoClient: The MongoDB client connection
+// - TransactionCollection: Reference to the transaction collection
 func InitMongoDB() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
